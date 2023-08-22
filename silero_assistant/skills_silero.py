c711f3153ac16027ba3b85ff2ee87fb69c58ba00
dsuragan002 <dauitsuragan002@gmail.com>
2023-08-22 22:08:12 +0600

    fix bugs

--- silero_assistant/skills_silero.py
+++ silero_assistant/skills_silero.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import webbrowser
 import asyncio
 import datetime
@@ -16,8 +15,8 @@ ok_always = 'path to sound pack/Jarvis Sound Pack_ru/Всегда к вашим 
 async def browser(active_assistant):
     '''Открывает браузер, указанный по умолчанию в системе, с указанным URL'''
     webbrowser.open('https://www.youtube.com/@dautmantis/videos', new=2)
-    if active_assistant in app.JARVIS_WAKE_WORDS:
-       app.play_audio(jarvis_yes)
+    if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+       app_silero.play_audio(jarvis_yes)
 
 async def browser_exit(active_assistant):
     '''Закрывает браузер'''
@@ -33,27 +32,27 @@ async def browser_exit(active_assistant):
             pass
 
     if closed:
-        if active_assistant in app.JARVIS_WAKE_WORDS:
-            app.play_audio(jarvis_yes)
+        if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+            app_silero.play_audio(jarvis_yes)
     else:
         print("Bot's response: Процесс браузера не найден.")
-        await app.assistant_tts('Процесс браузера не найден.', 'output.mp3', active_assistant)
+        await app_silero.assistant_tts('Процесс браузера не найден.', 'output.mp3', active_assistant)
         
 
 async def game(active_assistant):
     try:
         os.startfile("C:/Users/dauit/OneDrive/Рабочий стол/Hill Climb Racing.lnk")
-        if active_assistant in app.JARVIS_WAKE_WORDS:
-           app.play_audio(jarvis_yes)
+        if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+           app_silero.play_audio(jarvis_yes)
     except:
         print("Bot's response: Путь к файлу не найден, проверьте, правильный ли путь ?")
-        await app.assistant_tts('Путь к файлу не найден, проверьте, правильный ли путь?', 'output.mp3', active_assistant)
+        await app_silero.assistant_tts('Путь к файлу не найден, проверьте, правильный ли путь?', 'output.mp3', active_assistant)
         
 
 async def game_exit(active_assistant):
     try:
-        if active_assistant in app.JARVIS_WAKE_WORDS:
-            app.play_audio(ok_always)
+        if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+            app_silero.play_audio(ok_always)
 
         process_name = 'C:/Users/dauit/OneDrive/Рабочий стол/Hill Climb Racing.lnk'
 
@@ -65,16 +64,16 @@ async def game_exit(active_assistant):
                 pass
     except:
         print("Bot's response: Произошла ошибка при попытке закрытия игры.")
-        await app.assistant_tts('Произошла ошибка при попытке закрытия игры', 'output.mp3', active_assistant)
+        await app_silero.assistant_tts('Произошла ошибка при попытке закрытия игры', 'output.mp3', active_assistant)
         
 
 async def offpc(active_assistant):
     '''Отключает ПК под управлением Windows'''
-    if active_assistant in app.JARVIS_WAKE_WORDS:
-        app.play_audio(jarvis_off)
+    if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+        app_silero.play_audio(jarvis_off)
     os.system('shutdown /s /t 0')
     print("Bot's response: ПК был бы выключен, но команде # в коде мешает ;))) ")
-    await app.assistant_tts('ПК был бы выключен, но команде # в коде мешает ;)))', 'output.mp3', active_assistant)
+    await app_silero.assistant_tts('ПК был бы выключен, но команде # в коде мешает ;)))', 'output.mp3', active_assistant)
     
 
 async def weather(active_assistant):
@@ -83,18 +82,18 @@ async def weather(active_assistant):
         try:
             place = "Astana"
             # API in site https://openweathermap.org/
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={place}&appid=YOUR_API&units=metric&lang=ru'
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={place}&app_sileroid=YOUR_API&units=metric&lang=ru'
             async with session.get(url) as response:
                 data = await response.json()
                 status = data['weather'][0]['description']
                 temp = data['main']['temp']
         except aiohttp.ClientError as e:
             print(f"Произошла ошибка при попытке запроса к ресурсу API:{e}")
-            await app.synthesize_speech(f'Произошла ошибка при попытке запроса к ресурсу API:{e}', 'output.mp3', active_assistant)
+            await app_silero.synthesize_speech(f'Произошла ошибка при попытке запроса к ресурсу API:{e}', 'output.mp3', active_assistant)
         else:
             print(f"Bot's response: На улице {status} {temp} градусов")
-            await app.synthesize_speech(f"На улице {status} {temp} градусов", 'output.mp3', active_assistant)
-            app.play_audio('output.mp3')
+            await app_silero.synthesize_speech(f"На улице {status} {temp} градусов", 'output.mp3', active_assistant)
+            app_silero.play_audio('output.mp3')
             
 
 async def offBot():
@@ -103,11 +102,11 @@ async def offBot():
 
 async def passive(active_assistant):
     '''Пассив'''
-    if active_assistant in app.JARVIS_WAKE_WORDS:
-        app.play_audio(jarvis_yes)
+    if active_assistant in app_silero.JARVIS_WAKE_WORDS:
+        app_silero.play_audio(jarvis_yes)
     else:
         print("Bot's response: Я вас слышаю пассивно.")
-        await app.assistant_tts(f"Bot's response: Я вас слышаю пассивно.", 'output.mp3', active_assistant)
+        await app_silero.assistant_tts(f"Bot's response: Я вас слышаю пассивно.", 'output.mp3', active_assistant)
 
 async def time(active_assistant):
     '''Возвращает текущее время'''
@@ -115,11 +114,11 @@ async def time(active_assistant):
     current_time = now.strftime("%H:%M")
     print(f"Сейчас {current_time}")
     try:
-        await app.assistant_tts(f"Сейчас {current_time}", 'output.mp3', active_assistant)
+        await app_silero.assistant_tts(f"Сейчас {current_time}", 'output.mp3', active_assistant)
         
     except:
         print("Bot's response: Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона")
-        await app.assistant_tts('Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона', 'output.mp3', active_assistant)
+        await app_silero.assistant_tts('Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона', 'output.mp3', active_assistant)
         
 
 async def date(active_assistant):
@@ -128,9 +127,9 @@ async def date(active_assistant):
     current_date = now.strftime("%d.%m.%Y")
     print(f"Сегодня {current_date}")
     try:
-        await app.assistant_tts(f"Сегодня {current_date}", 'output.mp3', active_assistant)
+        await app_silero.assistant_tts(f"Сегодня {current_date}", 'output.mp3', active_assistant)
         
     except:
         print("Bot's response: Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона")
-        await app.assistant_tts('Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона', 'output.mp3', active_assistant)
+        await app_silero.assistant_tts('Произошла ошибка. Вы же можете сам посмотреть на экран компьютера или телефона', 'output.mp3', active_assistant)
         
